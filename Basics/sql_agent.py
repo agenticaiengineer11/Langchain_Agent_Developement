3d9a0b956e96d96a39182a904582b142296a02ec
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-08-15 07:42:05 +0500

    Database created using sqlite3 and tested using test.py file

---
+++ Basics/sql_agent.py
@@ -0,0 +1,36 @@
+print("====================SQL AGENT===================================")
+import sqlite3
+
+connection= sqlite3.connect("students.db")
+
+cursor = connection.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    department TEXT,
+    marks INTEGER
+)
+""")
+students = [
+    (1, "Ali", 21, "CS", 85),
+    (2, "Ahmed", 22, "IT", 78),
+    (3, "Noman", 22, "SE", 91),
+    (4, "Hassan", 21, "CS", 88),
+    (5, "Usman", 23, "IT", 74)
+]
+
+cursor.executemany(
+    """
+    INSERT OR REPLACE INTO students
+    (id, name, age, department, marks)
+    VALUES (?, ?, ?, ?, ?)
+    """,
+    students
+)
+
+connection.commit()
+
+connection.close()
